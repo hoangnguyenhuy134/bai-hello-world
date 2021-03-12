6a455ce824a91694deb03bad648b0c4827a76cd5
hoangnguyen <Hoangnguyenhuy134@gmail.com>
2021-03-12 22:24:08 +0700

    first commit

--- DSViecLam/src/navigations/index.js
+++ DSViecLam/src/navigations/index.js
@@ -1,8 +1,10 @@
+
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Login from '../screens/Login';
 import Products from '../screens/Products';
+import ProductDetail from '../screens/ProductDetail';
 
 const Stack = createStackNavigator();
 
@@ -14,6 +16,7 @@ function  RootNativation() {
             }}>
                 <Stack.Screen name="Login" component={Login} />
                 <Stack.Screen name="Products" component={Products} />
+                <Stack.Screen name="ProductDetail" component={ProductDetail} />
             </Stack.Navigator>
         </NavigationContainer>
     )
